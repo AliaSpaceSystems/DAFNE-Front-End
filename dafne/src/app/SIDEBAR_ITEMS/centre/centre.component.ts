4a3cfe4e48468e18dc8d6b82fc645817bfc448b1
spiderdab <77dab77@gmail.com>
2022-02-10 18:35:25 +0100

    test-centre-visibility

--- dafne/src/app/SIDEBAR_ITEMS/centre/centre.component.ts
+++ dafne/src/app/SIDEBAR_ITEMS/centre/centre.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Centre } from 'src/app/models/centre';
 import { AuthenticationService } from '../../services/authentication.service';
 
 @Component({
@@ -11,6 +12,8 @@ export class CentreComponent implements OnInit {
     name: "",
     color: "#ffffff"
   };
+  public centreList:any;
+  public currentCentre = this.localCentre;
   constructor(private authenticationService: AuthenticationService) { }
 
   ngOnInit(): void {
@@ -21,15 +24,49 @@ export class CentreComponent implements OnInit {
   getLocalCentre() {
     this.authenticationService.getAllCentres().subscribe(
       (res: object) => {
+        this.centreList = res;
+        this.centreList.sort(this.getSortOrder("id"));
+        this.setLocalFirst(this.centreList);
         if (Object.values(res).filter((x) => x.local == true)[0]) {
           this.localCentre = Object.values(res).filter((x) => x.local == true)[0];
+          this.currentCentre = this.localCentre;
         } else {
           this.localCentre = {
             name: "",
             color: "#ffffff"
           };
+          this.currentCentre = this.localCentre;
         }
       }
     );
+  }
+
+  setCentreVisibility(idx) {
+    this.currentCentre =this.centreList[idx];
+  }
+
+  /* Function to sort arrays of object: */
+  getSortOrder(prop) {
+    return function (a, b) {
+      if (a[prop] > b[prop]) {
+        return 1;
+      } else if (a[prop] < b[prop]) {
+        return -1;
+      }
+      return 0;
+    }
+  }
+
+  /* Function to put local first */
+  setLocalFirst(arr) {
+    let tempLocalId = arr.filter(a => a.local == true)[0].id;
+    for (var i = 0; i < arr.length; ++i) {
+      if (arr[i].id == tempLocalId) {
+        let tempObj = arr[i];
+        arr.splice(i, 1);
+        arr.unshift(tempObj);
+        break;
+      }
+    }
   }
 }
